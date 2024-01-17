f4c4900c6f01fb131c9c6a7a3d6361da8d9ca7cb
Jannatul Ferdous Nawrin <jannatulnawrin@gmail.com>
2024-01-17 18:25:07 +0600

    delete and edit implemented

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express= require('express')
 const app=express()
 const port = process.env.PORT || 4000;
+const cors=require('cors')
 require('dotenv').config()
 
 // middleware
+app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vqv383i.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,15 +37,50 @@ dbConnect()
 const recipeCollection = client.db("delishDb").collection("allRecipes");
 
 
+//for fetching all the recipies or individual recipie per query
 app.get('/allRecipes',async(req,res)=>{
-      const allRecipes=await recipeCollection.find().toArray()
+      const id=req.query.meal
+      let filter={}
+      if(id){
+        filter={_id:new ObjectId(id)}
+      }
+      const allRecipes=await recipeCollection.find(filter).toArray()
       res.send(allRecipes)
   })
 
+  app.patch('/allRecipes',async (req, res) => {
+    const id = req.query.recipeId;
+    const recipe=req.body
+    const query = { _id: new ObjectId(id) }
+    const updatedDoc = {
+      $set: {
+        name:recipe.name,
+        instruction:recipe.instruction,
+        ingredients:recipe.ingredients,
+        image:recipe.image
+      }
+    }
+
+    const result = await recipeCollection.updateOne(query, updatedDoc)
+    res.send(result);
+  })
+
+
+//for deleting a recipie
+app.delete('/allRecipes/:id',async(req,res)=>{
+    const id=req.params.id
+    // console.log(id,"from server")
+    const  filter={_id:new ObjectId(id)}
+    const recipe=await recipeCollection.deleteOne(filter)
+    // console.log("done")
+   
+    res.send(recipe)
+})
+
 
 app.get('/', async(req, res) => {
-    res.send('Delish is running')
-  })
+  res.send('Delish is running')
+})
 app.listen(port,()=>{
     console.log(`Delish is sitting on port ${port}`)
 })  
